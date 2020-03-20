54bcd27d7dea7c90f27bfe242d9072f1ab105412
Neil Williams <Williams.Sharode@gmail.com>
2020-03-20 11:56:08 -0400

    skeleton layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Filter from './Components/Filter';
+import Events from './Components/Events';
+import Event from './Components/Event';
+import EventMap from './Components/EventMap';
 
 function App() {
 	return (
-		<div className="App">
-			hello
-			<h1>hello</h1>
-		</div>
+		<Switch>
+			<Route path="/event/:id">
+				<Event />
+			</Route>
+			<Route exact path="/">
+				<Filter />
+				<Events />
+				<EventMap />
+			</Route>
+			<Route path="/*">
+				<Redirect to="/" />
+			</Route>
+		</Switch>
 	);
 }
 
